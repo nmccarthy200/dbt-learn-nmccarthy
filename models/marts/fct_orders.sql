ebd89ff8defb004853bd98192ac4cda744609cc9
Nicholas McCarthy <nmccarthy200@gmail.com>
2020-04-10 01:04:03 +0000

    2. Add test to project

--- models/marts/fct_orders.sql
+++ models/marts/fct_orders.sql
@@ -1,20 +1,37 @@
-with customers as (
- select * from {{ ref('stg_customers') }}
+with 
+
+payments as (
+select * from {{ref('stg_payment')}}
 ),
 orders as (
   select * from {{ ref('stg_orders') }}
 ),
-payment as (
-    select * from {{ ref('stg_payment') }}
-),
-final as (
+
+order_payments as (
+
     select
-    order_id,
-    order_date,
-    customer_id,
-    amount
-    from customers
-    inner join orders using(customer_ID)
-    inner join payment using(order_id)
+        order_id,
+        sum(amount) as amount
+
+    from payments
+
+    group by 1
+
+),
+
+final as (
+
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        orders.status,
+        coalesce(order_payments.amount, 0) as amount
+
+    from orders
+
+    left join order_payments using (order_id)
+
 )
-select * from final 
+
+select * from final
